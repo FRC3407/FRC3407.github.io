eb7fa70955af9fa1fb1f21e6706f4645b92f168a
bradyinnovation <bradybbangasser@gmail.com>
2022-12-01 19:46:24 -0600

    to fork

--- db/schemas/user.schema.ts
+++ db/schemas/user.schema.ts
@@ -1,3 +1,25 @@
-export default class Hello {
+import mongoose from "mongoose"
+
+interface IUser {
+    name: string
+    email: string
+    permLevel: "visitor" | "sponsor" | ""
     
 }
+
+const requiredString = {
+    type: String,
+    required: true
+}
+
+const UserSchema = new mongoose.Schema({
+    name: requiredString,
+    email: requiredString,
+    permLevel: requiredString,
+    primaryTeam: requiredString,
+    teams: Array,
+    isJohnLofton: {
+        type: Boolean,
+        default: false
+    }
+})
